a9b04e26703d78674c54074a054275255d7e9a47
Christophe Moinard <cmoinard@netc.fr>
2020-12-13 17:22:14 +0100

    Refactor Gets

--- src/Fleet/Router.fs
+++ src/Fleet/Router.fs
@@ -1,26 +1,39 @@
 module Fleet.Router
 
+open Giraffe
 open Saturn
+open Saturn.ControllerHelpers.Response
 open Utils.Web.JsonHelpers
 open FSharp.Data
-open Fleet
+open FSharp.Control.Tasks.V2
 
 type Appsettings = JsonProvider<"appsettings.json">
+
+// reminder        
+// type HttpFuncResult = Task<HttpContext option>
+// type HttpFunc = HttpContext -> HttpFuncResult
+// type HttpHandler = HttpFunc -> HttpContext -> HttpFuncResult
 
 let locomotive =    
     let conf = Appsettings.Load("appsettings.json")
     let connStr = conf.ConnectionStrings.Fleet
     
-    let getAll =
+    let getAll : HttpHandler =
         Database.getAll connStr
         |> toJson'
         
-    // TODO : How to convert Option<> to NotFound ?
-    let getById id =
-        Database.getById connStr id
-        |> toJson'
+    let getById id : HttpHandler =
+        fun next ctx -> task {
+            let! locomotive = Database.getById connStr id
+            
+            return!
+                match locomotive with
+                | None -> notFound ctx ()
+                | Some l -> json l next ctx
+        }
     
     router {
         get "" getAll
         getf "/%i" getById
     }
+    
